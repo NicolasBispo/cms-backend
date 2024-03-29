7eba4bae67ed2ba7a9a1d8514c86d9093d9c074b
nicolasbispo <nbm.personalwork@gmail.com>
2024-03-29 13:06:48 -0300

    added: middlewares to each route

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { CategoryController } from "../controllers/categoryController";
+import { isAuth } from "../middlewares/authMiddleware";
+import { setCategory } from "../middlewares/categoriesMiddlewares";
 const router = express.Router();
 
-router.get("/", CategoryController.index);
-router.get("/:id", CategoryController.show);
-router.post("/", CategoryController.create);
-router.put("/:id", CategoryController.update);
-router.delete("/:id", CategoryController.delete);
+router.get("/", isAuth, CategoryController.index);
+router.get("/:id", isAuth, CategoryController.show);
+router.post("/", isAuth, CategoryController.create);
+router.put("/:id", isAuth, setCategory, CategoryController.update);
+router.delete("/:id", isAuth, CategoryController.delete);
 
-export const categoryRouter = router
+export const categoryRouter = router;
--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { CommentController } from "../controllers/commentController";
+import { hasPermissionOnComment, setComment } from "../middlewares/commentsMiddlewares";
+import { isAuth } from "../middlewares/authMiddleware";
 const router = express.Router();
 
 router.get("/", CommentController.index);
 router.get("/:id", CommentController.show);
-router.post("/", CommentController.create);
-router.put("/:id", CommentController.update);
-router.delete("/:id", CommentController.delete);
+router.post("/", isAuth, CommentController.create);
+router.put("/:id", isAuth, setComment, hasPermissionOnComment, CommentController.update);
+router.delete("/:id", isAuth, setComment, hasPermissionOnComment, CommentController.delete);
 
 export const commentRouter = router
