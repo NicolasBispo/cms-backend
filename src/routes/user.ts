cb9dc801bf88d35eb9ee955f0fef3221c8b148a9
nicolasbispo <nbm.personalwork@gmail.com>
2024-03-29 22:45:09 -0300

    added: validator to users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { UserController as userController } from "../controllers/userController";
 import { setUser } from "../middlewares/usersMiddlewares";
 import { isAuth } from "../middlewares/authMiddleware";
+import { validateCreateUser } from "../middlewares/validators/user";
 const router = express.Router();
 
 router.get("/", userController.index);
 router.get("/:id", userController.show);
-router.post("/", isAuth, userController.create);
-router.put("/:id", isAuth, setUser, userController.update);
+router.post("/", isAuth, validateCreateUser, userController.create);
+router.put("/:id", isAuth, setUser, validateCreateUser, userController.update);
 router.delete("/:id", isAuth, userController.delete);
 
 export const userRouter = router;
